a258da5746ce66b06c8cba55356cbb87d2e2e285
MordockDun <daniel@koller.dk>
2026-01-04 20:44:55 +0100

    Add CSRF token meta tag and configure Axios for CSRF handling

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,9 +3,18 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import axios from 'axios';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+
+// Configure Axios to send CSRF token with every request
+const csrfToken = document.head.querySelector('meta[name="csrf-token"]');
+if (csrfToken) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken.getAttribute('content');
+}
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Accept'] = 'application/json';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
